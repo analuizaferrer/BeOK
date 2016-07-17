deebda99061fcb4fdac7013f24c48e5a8c29a8a8
Gabriella Lopes <GabriellaBLopes@Gabriellas-MacBook-Pro.local>
2016-07-17 17:13:12 -0300

    animação com IN HOLD OUT

--- BeOK/FirstViewController.swift
+++ BeOK/FirstViewController.swift
@@ -11,77 +11,110 @@ import AVKit
 import AVFoundation
 
 class FirstViewController: UIViewController {
-
-    @IBOutlet weak var copingMessage: UILabel!
-    
-    @IBOutlet weak var videoView: UIView!
-    
-    var videoName: String!
-    
+  
+  @IBOutlet weak var copingMessage: UILabel!
+  @IBOutlet weak var centerButton: UIButton!
+  @IBOutlet weak var outButton: UIButton!
+  
+  
+  @IBOutlet weak var videoView: UIView!
+  
+  
+  var videoName: String!
+  
+  var breathingTimer = NSTimer()
+  
+  
   @IBOutlet weak var breathingCircle: UIImageView!
   
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-      
-
-        
-    }
-
+    self.outButton.hidden = true
+    //self.breathingCircle.transform = CGAffineTransformMakeScale(1.5, 1.5)
+    
+    
+  }
+  
   
   
   func animateBreathing() {
     
     let bigger:CGAffineTransform = CGAffineTransformMakeScale(2.0, 2.0)
-    let smaller:CGAffineTransform = CGAffineTransformMakeScale(0.5, 0.5)
-
+    let smaller:CGAffineTransform = CGAffineTransformMakeScale(0.7, 0.7)
+    
     let center:CGPoint = breathingCircle.center
     
+    
+    
     UIView.animateWithDuration(4.0, delay: 0.0, options: [], animations: {
+      
+      self.centerButton.hidden = false
+      self.outButton.hidden = true
       
       self.breathingCircle.transform = bigger
       self.breathingCircle.center = center
       
-
+      
       }, completion: { _ in
         
-        UIView.animateWithDuration(7.0, delay: 3.0, options: [], animations: {
+        
+        UIView.animateWithDuration(3.0, delay: 0.0, options: [], animations: {
           
-          self.breathingCircle.transform = smaller
-          self.breathingCircle.center = center
+          self.centerButton.setTitle("HOLD", forState: .Normal)
           
+          if self.centerButton.alpha == 1.0{
+          self.centerButton.alpha = 0.9
+          } else {
+            self.centerButton.alpha = 1.0
+          }
           
-          }, completion: nil)
+          }, completion: { _ in
+            
+            self.centerButton.hidden = true
+            self.outButton.hidden = false
+            
+            UIView.animateWithDuration(7.0, delay: 0.0, options: [], animations: {
+              
+              self.breathingCircle.transform = smaller
+              self.breathingCircle.center = center
+              self.centerButton.setTitle("IN", forState: .Normal)
+
+              
+              
+              
+              }, completion: nil)
+            
+            
+        })
         
     })
-
-    
     
     
   }
-
+  
   
   override func viewDidAppear(animated: Bool) {
     
-
+    breathingTimer = NSTimer.scheduledTimerWithTimeInterval(15.0, target: self, selector: #selector(FirstViewController.animateBreathing), userInfo: nil, repeats: true)
     
-    let timer = NSTimer.scheduledTimerWithTimeInterval(15.0, target: self, selector: #selector(FirstViewController.animateBreathing), userInfo: nil, repeats: true)
-    timer.fire()
-  
-    
-
+    breathingTimer.fire()
     
   }
   
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-//    override func viewDidLayoutSubviews() {
-//        playerLayer.frame = videoView.bounds
-//    }
-    
+  
+
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+  
+  //    override func viewDidLayoutSubviews() {
+  //        playerLayer.frame = videoView.bounds
+  //    }
+  
 }
 
