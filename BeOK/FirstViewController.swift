d2532e6a4cee05d7b237704859548629c1f49f39
Helena Leitão <hmleit1@gmail.com>
2016-07-18 11:56:11 -0300

    cicadas noise

--- BeOK/FirstViewController.swift
+++ BeOK/FirstViewController.swift
@@ -11,110 +11,108 @@ import AVKit
 import AVFoundation
 
 class FirstViewController: UIViewController {
-  
-  @IBOutlet weak var copingMessage: UILabel!
-  @IBOutlet weak var centerButton: UIButton!
-  @IBOutlet weak var outButton: UIButton!
-  
-  
-  @IBOutlet weak var videoView: UIView!
-  
-  
-  var videoName: String!
-  
-  var breathingTimer = NSTimer()
-  
-  
-  @IBOutlet weak var breathingCircle: UIImageView!
-  
-  
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
     
-    self.outButton.hidden = true
-    //self.breathingCircle.transform = CGAffineTransformMakeScale(1.5, 1.5)
+    @IBOutlet weak var copingMessage: UILabel!
+    @IBOutlet weak var centerButton: UIButton!
+    @IBOutlet weak var outButton: UIButton!
     
     
-  }
-  
-  
-  
-  func animateBreathing() {
-    
-    let bigger:CGAffineTransform = CGAffineTransformMakeScale(2.0, 2.0)
-    let smaller:CGAffineTransform = CGAffineTransformMakeScale(0.7, 0.7)
-    
-    let center:CGPoint = breathingCircle.center
+    @IBOutlet weak var videoView: UIView!
     
     
+    var audioURL = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("Cicadas-noise", ofType: "mp3")!)
     
-    UIView.animateWithDuration(4.0, delay: 0.0, options: [], animations: {
-      
-      self.centerButton.hidden = false
-      self.outButton.hidden = true
-      
-      self.breathingCircle.transform = bigger
-      self.breathingCircle.center = center
-      
-      
-      }, completion: { _ in
+    var audioPlayer = AVAudioPlayer()
+    
+    var breathingTimer = NSTimer()
+    
+    
+    @IBOutlet weak var breathingCircle: UIImageView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
         
+        self.outButton.hidden = true
+        //self.breathingCircle.transform = CGAffineTransformMakeScale(1.5, 1.5)
         
-        UIView.animateWithDuration(3.0, delay: 0.0, options: [], animations: {
-          
-          self.centerButton.setTitle("HOLD", forState: .Normal)
-          
-          if self.centerButton.alpha == 1.0{
-          self.centerButton.alpha = 0.9
-          } else {
-            self.centerButton.alpha = 1.0
-          }
-          
-          }, completion: { _ in
+    }
+    
+    func animateBreathing() {
+        
+        let bigger:CGAffineTransform = CGAffineTransformMakeScale(2.0, 2.0)
+        let smaller:CGAffineTransform = CGAffineTransformMakeScale(0.7, 0.7)
+        
+        let center:CGPoint = breathingCircle.center
+        
+        UIView.animateWithDuration(4.0, delay: 0.0, options: [], animations: {
             
-            self.centerButton.hidden = true
-            self.outButton.hidden = false
+            self.centerButton.hidden = false
+            self.outButton.hidden = true
             
-            UIView.animateWithDuration(7.0, delay: 0.0, options: [], animations: {
-              
-              self.breathingCircle.transform = smaller
-              self.breathingCircle.center = center
-              self.centerButton.setTitle("IN", forState: .Normal)
-
-              
-              
-              
-              }, completion: nil)
+            self.breathingCircle.transform = bigger
+            self.breathingCircle.center = center
             
             
+            }, completion: { _ in
+                
+                
+                UIView.animateWithDuration(3.0, delay: 0.0, options: [], animations: {
+                    
+                    self.centerButton.setTitle("HOLD", forState: .Normal)
+                    
+                    if self.centerButton.alpha == 1.0{
+                        self.centerButton.alpha = 0.9
+                    } else {
+                        self.centerButton.alpha = 1.0
+                    }
+                    
+                    }, completion: { _ in
+                        
+                        self.centerButton.hidden = true
+                        self.outButton.hidden = false
+                        
+                        UIView.animateWithDuration(7.0, delay: 0.0, options: [], animations: {
+                            
+                            self.breathingCircle.transform = smaller
+                            self.breathingCircle.center = center
+                            self.centerButton.setTitle("IN", forState: .Normal)
+                        
+                        }, completion: nil)
+                })
         })
+    }
+    
+    
+    override func viewDidAppear(animated: Bool) {
         
-    })
+        breathingTimer = NSTimer.scheduledTimerWithTimeInterval(15.0, target: self, selector: #selector(FirstViewController.animateBreathing), userInfo: nil, repeats: true)
+        
+        breathingTimer.fire()
+        
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOfURL: audioURL, fileTypeHint: nil)
+            audioPlayer.play()
+        }
+        catch let error as NSError {
+            print(error.description)
+        }
+    }
+    
+    override func viewDidDisappear(animated: Bool) {
+        audioPlayer.stop()
+    }
     
     
-  }
-  
-  
-  override func viewDidAppear(animated: Bool) {
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
     
-    breathingTimer = NSTimer.scheduledTimerWithTimeInterval(15.0, target: self, selector: #selector(FirstViewController.animateBreathing), userInfo: nil, repeats: true)
+    //    override func viewDidLayoutSubviews() {
+    //        playerLayer.frame = videoView.bounds
+    //    }
     
-    breathingTimer.fire()
-    
-  }
-  
-  
-
-  
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
-  
-  //    override func viewDidLayoutSubviews() {
-  //        playerLayer.frame = videoView.bounds
-  //    }
-  
 }
 
