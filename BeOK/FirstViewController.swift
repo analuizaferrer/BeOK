ac61ea5cebee12ad72bd7cf2fbd6478d0857edf3
Gabriella Lopes <GabriellaBLopes@Gabriellas-MacBook-Pro.local>
2016-07-19 18:17:39 -0300

    novos sons + coping messages esquematizadas

--- BeOK/FirstViewController.swift
+++ BeOK/FirstViewController.swift
@@ -11,100 +11,111 @@ import AVKit
 import AVFoundation
 
 class FirstViewController: UIViewController {
+  
+  @IBOutlet weak var copingMessage: UILabel!
+  @IBOutlet weak var centerButton: UIButton!
+  @IBOutlet weak var outButton: UIButton!
+  
+  var audioURL = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("Cicadas noise", ofType: "mp3")!)
+  
+  var audioPlayer = AVAudioPlayer()
+  
+  var breathingTimer = NSTimer()
+  
+  @IBOutlet weak var breathingCircle: UIImageView!
+  
+  let temporaryCopingMessages = ["It's gonna be okay.","We all love you very much.","Look around: everything is just fine.","Pay attention to your breathing."]
+  var index = 0
+  
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
     
-    @IBOutlet weak var copingMessage: UILabel!
-    @IBOutlet weak var centerButton: UIButton!
-    @IBOutlet weak var outButton: UIButton!
+    self.outButton.hidden = true
+    //self.breathingCircle.transform = CGAffineTransformMakeScale(1.5, 1.5)
     
-    var audioURL = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("Cicadas noise", ofType: "mp3")!)
+  }
+  
+  func animateBreathing() {
     
-    var audioPlayer = AVAudioPlayer()
+    let bigger:CGAffineTransform = CGAffineTransformMakeScale(2.0, 2.0)
+    let smaller:CGAffineTransform = CGAffineTransformMakeScale(0.7, 0.7)
     
-    var breathingTimer = NSTimer()
+    let center:CGPoint = breathingCircle.center
     
-    @IBOutlet weak var breathingCircle: UIImageView!
-    
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        
-        self.outButton.hidden = true
-        //self.breathingCircle.transform = CGAffineTransformMakeScale(1.5, 1.5)
-        
+    if index == temporaryCopingMessages.count {
+      index = 0
     }
     
-    func animateBreathing() {
-        
-        let bigger:CGAffineTransform = CGAffineTransformMakeScale(2.0, 2.0)
-        let smaller:CGAffineTransform = CGAffineTransformMakeScale(0.7, 0.7)
-        
-        let center:CGPoint = breathingCircle.center
+    self.copingMessage.text = temporaryCopingMessages[index]
+    
+    
+    UIView.animateWithDuration(4.0, delay: 0.0, options: [], animations: {
+      
+      self.centerButton.hidden = false
+      self.outButton.hidden = true
+      
+      self.breathingCircle.transform = bigger
+      self.breathingCircle.center = center
       
       
-        UIView.animateWithDuration(4.0, delay: 0.0, options: [], animations: {
+      }, completion: { _ in
+        
+        
+        UIView.animateWithDuration(3.0, delay: 0.0, options: [], animations: {
+          
+          self.centerButton.setTitle("HOLD", forState: .Normal)
+          
+          if self.centerButton.alpha == 1.0{
+            self.centerButton.alpha = 0.9
+          } else {
+            self.centerButton.alpha = 1.0
+          }
+          
+          }, completion: { _ in
             
-            self.centerButton.hidden = false
-            self.outButton.hidden = true
+            self.centerButton.hidden = true
+            self.outButton.hidden = false
             
-            self.breathingCircle.transform = bigger
-            self.breathingCircle.center = center
-            
-            
-            }, completion: { _ in
-                
-                
-                UIView.animateWithDuration(3.0, delay: 0.0, options: [], animations: {
-                    
-                    self.centerButton.setTitle("HOLD", forState: .Normal)
-                    
-                    if self.centerButton.alpha == 1.0{
-                        self.centerButton.alpha = 0.9
-                    } else {
-                        self.centerButton.alpha = 1.0
-                    }
-                    
-                    }, completion: { _ in
-                        
-                        self.centerButton.hidden = true
-                        self.outButton.hidden = false
-                        
-                        UIView.animateWithDuration(7.0, delay: 0.0, options: [], animations: {
-                            
-                            self.breathingCircle.transform = smaller
-                            self.breathingCircle.center = center
-                            self.centerButton.setTitle("IN", forState: .Normal)
-                        
-                        }, completion: nil)
-                })
+            UIView.animateWithDuration(7.0, delay: 0.0, options: [], animations: {
+              
+              self.breathingCircle.transform = smaller
+              self.breathingCircle.center = center
+              self.centerButton.setTitle("IN", forState: .Normal)
+              
+              }, completion: nil)
         })
+    })
+    
+    self.index += 1
+  }
+  
+  
+  override func viewDidAppear(animated: Bool) {
+    
+    breathingTimer = NSTimer.scheduledTimerWithTimeInterval(15.0, target: self, selector: #selector(FirstViewController.animateBreathing), userInfo: nil, repeats: true)
+    
+    breathingTimer.fire()
+    
+    do {
+      audioPlayer = try AVAudioPlayer(contentsOfURL: audioURL, fileTypeHint: nil)
+      audioPlayer.play()
     }
-    
-    
-    override func viewDidAppear(animated: Bool) {
-        
-        breathingTimer = NSTimer.scheduledTimerWithTimeInterval(15.0, target: self, selector: #selector(FirstViewController.animateBreathing), userInfo: nil, repeats: true)
-        
-        breathingTimer.fire()
-        
-        do {
-            audioPlayer = try AVAudioPlayer(contentsOfURL: audioURL, fileTypeHint: nil)
-            audioPlayer.play()
-        }
-        catch let error as NSError {
-            print(error.description)
-        }
+    catch let error as NSError {
+      print(error.description)
     }
-    
-    override func viewDidDisappear(animated: Bool) {
-        audioPlayer.stop()
-        breathingTimer.invalidate()
-    }
-    
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+  }
+  
+  override func viewDidDisappear(animated: Bool) {
+    audioPlayer.stop()
+    breathingTimer.invalidate()
+  }
+  
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
 }
 
--- BeOK/SoundsTableViewController.swift
+++ BeOK/SoundsTableViewController.swift
@@ -10,86 +10,91 @@ import UIKit
 import AVFoundation
 
 class SoundsTableViewController: UITableViewController {
+  
+  var sounds: [String] = []
+  
+  var urls: [NSURL] = []
+  
+  var checked: [Bool] = []
+  
+  let cicadasURL = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("Cicadas noise", ofType: "mp3")!)
+  let rainforestURL = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("Rainforest sounds", ofType: "mp3")!)
+  
+  let lakeURL = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("lake", ofType: "wav")!)
+  //let chuvaURL = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("chuva", ofType: "wav")!)
+  let wavesURL = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("waves", ofType: "wav")!)
+  
+  
+  var audioPlayer = AVAudioPlayer()
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
     
-    var sounds: [String] = []
+    sounds += ["Rainforest sounds", "Cicadas noise", "Lake", "Waves"]
+    urls += [rainforestURL, cicadasURL, lakeURL, wavesURL]
+    checked += [false, false, false, false, false]
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+  
+  // MARK: - Table view data source
+  
+  override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    // #warning Incomplete implementation, return the number of rows
+    return sounds.count
+  }
+  
+  
+  override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
     
-    var urls: [NSURL] = []
+    UITableViewCell.appearance().tintColor = UIColor(red:0.26, green:0.29, blue:0.61, alpha:1.0)
     
-    var checked: [Bool] = []
+    cell.textLabel?.text = sounds[indexPath.row]
     
-    let cicadasURL = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("Cicadas noise", ofType: "mp3")!)
-    let rainforestURL = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("Rainforest sounds", ofType: "mp3")!)
-    
-    var audioPlayer = AVAudioPlayer()
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        sounds += ["Rainforest sounds", "Cicadas noise"]
-        urls += [rainforestURL, cicadasURL]
-        checked += [false, false]
+    if !checked[indexPath.row] {
+      cell.accessoryType = .None
+    } else if checked[indexPath.row] {
+      cell.accessoryType = .Checkmark
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    return cell
+  }
+  
+  override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
     
-    // MARK: - Table view data source
-    
-    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return sounds.count
-    }
-    
-    
-    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
+    if let cell = tableView.cellForRowAtIndexPath(indexPath) {
+      do {
+        audioPlayer = try AVAudioPlayer(contentsOfURL: urls[indexPath.row], fileTypeHint: nil)
+        audioPlayer.prepareToPlay()
+        audioPlayer.play()
+      }
+      catch let error as NSError {
+        print(error.description)
+      }
+      
+      cell.selectionStyle = UITableViewCellSelectionStyle.None
+      
+      if cell.accessoryType == .None {
         
-        UITableViewCell.appearance().tintColor = UIColor(red:0.26, green:0.29, blue:0.61, alpha:1.0)
+        cell.accessoryType = .Checkmark
+        checked[indexPath.row] = true
         
-        cell.textLabel?.text = sounds[indexPath.row]
-        
-        if !checked[indexPath.row] {
-            cell.accessoryType = .None
-        } else if checked[indexPath.row] {
-            cell.accessoryType = .Checkmark
+        var i = 0
+        while i < checked.count {
+          if i != indexPath.row {
+            checked[i] = false
+          }
+          i += 1
         }
-        
-        return cell
+      }
     }
-    
-    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-        
-        if let cell = tableView.cellForRowAtIndexPath(indexPath) {
-            do {
-                audioPlayer = try AVAudioPlayer(contentsOfURL: urls[indexPath.row], fileTypeHint: nil)
-                audioPlayer.prepareToPlay()
-                audioPlayer.play()
-            }
-            catch let error as NSError {
-                print(error.description)
-            }
-            
-            cell.selectionStyle = UITableViewCellSelectionStyle.None
-            
-            if cell.accessoryType == .None {
-               
-                cell.accessoryType = .Checkmark
-                checked[indexPath.row] = true
-               
-                var i = 0
-                while i < checked.count {
-                    if i != indexPath.row {
-                        checked[i] = false
-                    }
-                    i += 1
-                }
-            }
-        }
-    }
-    
-    override func tableView(tableView: UITableView, didDeselectRowAtIndexPath indexPath: NSIndexPath) {
-        tableView.cellForRowAtIndexPath(indexPath)?.accessoryType = .None
-    }
+  }
+  
+  override func tableView(tableView: UITableView, didDeselectRowAtIndexPath indexPath: NSIndexPath) {
+    tableView.cellForRowAtIndexPath(indexPath)?.accessoryType = .None
+  }
 }
