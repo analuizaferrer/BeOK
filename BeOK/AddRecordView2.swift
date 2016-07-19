aa145a1ee7a83350b68013bb135f8c097e31d1f6
Helena Leitão <hmleit1@gmail.com>
2016-07-19 01:18:30 -0300

    sounds on cells

--- BeOK/AddRecordView2.swift
+++ BeOK/AddRecordView2.swift
@@ -78,5 +78,4 @@ class AddRecordView2: UIView, UITableViewDelegate, UITableViewDataSource {
             }
         }    
     }
-
 }
--- BeOK/SoundsTableViewController.swift
+++ BeOK/SoundsTableViewController.swift
@@ -10,98 +10,80 @@ import UIKit
 import AVFoundation
 
 class SoundsTableViewController: UITableViewController {
-
+    
     var sounds: [String] = []
+    
+    var urls: [NSURL] = []
+    
+    var checked: [Bool] = [true, false]
+    
+    let cicadasURL = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("Cicadas noise", ofType: "mp3")!)
+    let rainforestURL = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("Rainforest sounds", ofType: "mp3")!)
+    
+    var audioPlayer = AVAudioPlayer()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
-
+        
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem()
         sounds += ["Rainforest sounds", "Cicadas noise"]
+        urls += [rainforestURL, cicadasURL]
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     // MARK: - Table view data source
-
+    
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
         return sounds.count
     }
-
+    
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
-
+        
+        UITableViewCell.appearance().tintColor = UIColor(red:0.26, green:0.29, blue:0.61, alpha:1.0)
+        
         cell.textLabel?.text = sounds[indexPath.row]
-
+        
+        if !checked[indexPath.row] {
+            cell.accessoryType = .None
+        } else if checked[indexPath.row] {
+            cell.accessoryType = .Checkmark
+        }
+        
         return cell
     }
     
     override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-        let audioURL = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource(sounds[indexPath.row], ofType: "mp3")!)
-        var audioPlayer = AVAudioPlayer()
         
-        do {
-            audioPlayer = try AVAudioPlayer(contentsOfURL: audioURL, fileTypeHint: nil)
-            audioPlayer.prepareToPlay()
-            audioPlayer.play()
+        if let cell = tableView.cellForRowAtIndexPath(indexPath) {
+            do {
+                audioPlayer = try AVAudioPlayer(contentsOfURL: urls[indexPath.row], fileTypeHint: nil)
+                audioPlayer.prepareToPlay()
+                audioPlayer.play()
+            }
+            catch let error as NSError {
+                print(error.description)
+            }
+            
+            cell.selectionStyle = UITableViewCellSelectionStyle.None
+            
+            if cell.accessoryType == .Checkmark {
+                cell.accessoryType = .None
+                checked[indexPath.row] = false
+            } else {
+                cell.accessoryType = .Checkmark
+                checked[indexPath.row] = true
+            }
         }
-        catch let error as NSError {
-            print(error.description)
-        }
     }
-    
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
