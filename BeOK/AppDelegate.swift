5f6e5cc3e779c4bade7e2037ab04b18bedb0d63b
Helena Leitão <hmleit1@gmail.com>
2016-07-26 23:09:27 -0300

    notification badge number

--- BeOK/AppDelegate.swift
+++ BeOK/AppDelegate.swift
@@ -52,7 +52,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                 saveDefaultSoundsInCoreData(i.0, isActive: i.1)
             }
         }
-
+        
         return true
     }
     
@@ -117,30 +117,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             
         }
     }
-
-
-    func applicationWillResignActive(application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-    }
-
-    func applicationDidEnterBackground(application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
-
-    func applicationWillEnterForeground(application: UIApplication) {
-        // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-    }
-
-    func applicationDidBecomeActive(application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
-    func applicationWillTerminate(application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
-    
     
     // MARK: - Core Data stack
     
@@ -230,11 +206,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         if application.applicationState == .Active {
         
         }
-//        } else {
-//            
-//        }
         
         self.takeActionWithNotification(notification)
+        application.applicationIconBadgeNumber = 0
     }
     
     func takeActionWithNotification(localNotification: UILocalNotification) {
@@ -252,7 +226,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         alertController.addAction(sureAction)
         
         self.window?.rootViewController?.presentViewController(alertController, animated: true, completion: nil)
-        localNotification.applicationIconBadgeNumber = 0
     }
 }
 
